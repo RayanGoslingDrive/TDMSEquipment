b1ebd3a317d38b75287e7ca8106aef11c461736e
RyanGosling <Drive2011@dsas.asd>
2024-07-12 18:00:15 +0600

    fix

--- TDMSEquipment/Commands/DocAddCommand.cs
+++ TDMSEquipment/Commands/DocAddCommand.cs
@@ -16,7 +16,7 @@ namespace TDMSEquipment
     /// Демонстрационная команда, которая будет зарегистрирована <br/>
     /// При выполнении команды выполнится метод <see cref="Execute"/>
     /// </summary>
-    [TdmsApiCommand("CMD_COMMAND_ADD_DOC", description: "добавить документацию", roles: "SYSADMIN",icon: "44",objectDefs:"OBJECT_DOCUMENTATION,OBJECT_DOC_DIV,OBJECT_FOLDER")]
+    [TdmsApiCommand("CMD_COMMAND_ADD_DOC", description: "Добавить/изменить документацию", roles: "SYSADMIN",icon: "44",objectDefs:"OBJECT_DOCUMENTATION,OBJECT_DOC_DIV,OBJECT_FOLDER")]
     public class DocAddCommand : CommandBase
     {
         private readonly TDMSObject ThisObject;
@@ -49,20 +49,20 @@ namespace TDMSEquipment
                 }
             }
             if (doc.Show() == true)
-            {
-                if (doc.Controls["BUTTON_MAKE_AS_DOC"].Enabled == false)
+            { 
+                if (doc.Controls["BUTTON_MAKE_AS_DOC"].Enabled == false && (ThisObject.ObjectDefName == "OBJECT_DOC_DIV" || ThisObject.ObjectDefName == "OBJECT_DOCUMENTATION"))
                 {
                     TDMSObject tobj = ThisObject.Content.Create("OBJECT_DOCUMENTATION");
                     tobj.Attributes["ATTR_DOC_TITLE"].Value = doc.Controls["ATTR_DOC_TITLE"].Value;
                     tobj.Attributes["ATTR_TEXT"].Value = doc.Controls["ATTR_TEXT"].Value;
                 }
-                else if (doc.Controls["BUTTON_MAKE_AS_PAGE"].Enabled == false)
+                else if (doc.Controls["BUTTON_MAKE_AS_PAGE"].Enabled == false && (ThisObject.ObjectDefName == "OBJECT_DOC_DIV" || ThisObject.ObjectDefName == "OBJECT_DOCUMENTATION"))
                 {
                     TDMSObject tobj = ThisObject.Content.Create("OBJECT_DOC_DIV");
                     tobj.Attributes["ATTR_DOC_TITLE"].Value = doc.Controls["ATTR_DOC_TITLE"].Value;
                     tobj.Attributes["ATTR_TEXT"].Value = doc.Controls["ATTR_TEXT"].Value;
                 }
-                else if (doc.Controls["BUTTON_MAKE_AS_PAGE"].Enabled == true && doc.Controls["BUTTON_MAKE_AS_DOC"].Enabled == true)
+                else if (doc.Controls["BUTTON_MAKE_AS_PAGE"].Enabled == true && doc.Controls["BUTTON_MAKE_AS_DOC"].Enabled == true && (ThisObject.ObjectDefName == "OBJECT_DOC_DIV" || ThisObject.ObjectDefName == "OBJECT_DOCUMENTATION"))
                 {
                     ThisObject.Attributes["ATTR_DOC_TITLE"].Value = doc.Controls["ATTR_DOC_TITLE"].Value;
                     ThisObject.Attributes["ATTR_TEXT"].Value = doc.Controls["ATTR_TEXT"].Value;
